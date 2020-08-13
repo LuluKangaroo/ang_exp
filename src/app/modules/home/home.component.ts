231c2ac30760d6c19facda2ddf638dd9af0f31f5
Lulu Kangaroo <lululiu85@gmail.com>
2020-08-13 11:57:32 -0600

    animation practice

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
+    animations: [
+        trigger('myAwesomeAnimation', [
+            state('small', style({
+                fontSize: '10px'
+            })),
+            state('large', style({
+                fontSize: '20px'
+            })),
+            transition('small => void', animate('200ms ease-in')),
+            transition('large => void', animate('900ms ease-in')),
+            transition('void => small', animate('2000ms ease-in')),
+            transition('void => large', animate('9000ms ease-in')),
+        ])
+    ]
 })
 export class HomeComponent implements OnInit {
     title = 'home-page'
+    state: string = 'smallguy';
     
     ngOnInit(){
         // Maybe animation/transitions here???
+    }
+
+    animateMe() {
+        this.state = (this.state === 'small' ? 'large' : 'small');
     }
 }
